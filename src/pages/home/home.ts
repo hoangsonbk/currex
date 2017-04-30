005bd5374f264604a3d982745cc1eaed4fe05386
hoangsontemp@gmail.com <hoangsontemp@gmail.com>
2017-05-01 00:16:15 +0900

    new update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,35 +4,37 @@ import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { CurrencyExchProvider } from '../../providers/currency-exch-provider'
 
+import {Injectable} from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
   providers: [CurrencyExchProvider]
 })
+@Injectable()
 export class HomePage {
 	baseCurrency: string;
-	baseCurrencyValue: string;
+	baseCurrencyValue: any;
 	targetCurrency: string;
-	targetCurrencyValue: string;
+	targetCurrencyValue: any;
 	currencySelectAlertOpen: boolean;
 	LValue: string;
 	RValue: string;
 	result: any;
+	currencies: any;
 
 
-	constructor(public navCtrl: NavController, public alertController: AlertController, public exchProvder: CurrencyExchProvider) {
+	constructor(public navCtrl: NavController, public alertController: AlertController, public exchProvder: CurrencyExchProvider, http: Http) {
     	this.baseCurrency = 'USD';
-    	this.targetCurrency = 'USD';
+    	this.targetCurrency = 'JPY';
     	this.exchProvder.baseCur = 'USD';
     	this.exchProvder.targetCur = 'JPY';
-    	//this.loadCurExchProider();
-	}
-
-	loadCurExchProider(){
-		this.exchProvder.load().then(data => {
-			this.result = JSON.stringify(data);
-			console.log('>>>Result:' + this.result);
-		});
+    	this.baseCurrencyValue = 1;
+    	this.requestExchProvider();
+    	this.currencies =  http.get('assets/json/currencies.json').map(res => res.json());
+    	console.log('>>>currencies:' + JSON.stringify(this.currencies));
 	}
 
 	baseCurrencyButtonClicked(event){
@@ -43,11 +45,15 @@ export class HomePage {
 		this.selectCurrency("targetCurrency");
 	}
 
-	update(){
+	requestExchProvider(){
 		this.exchProvder.baseCur = this.baseCurrency;
         this.exchProvder.targetCur = this.targetCurrency;
-        console.log('>>>this.exchProvder.baseCur:' + this.exchProvder.baseCur);
-		this.loadCurExchProider();
+        //console.log('>>>this.exchProvder.baseCur:' + this.exchProvder.baseCur);
+		this.exchProvder.load().then(data => {
+			this.result = data;
+			console.log('>>>@home-Request result:' + JSON.stringify(this.result));
+			this.targetCurrencyValue = (this.baseCurrencyValue * this.result).toFixed(2);
+		});
 	}
 
 	selectCurrency(source){
@@ -65,21 +71,19 @@ export class HomePage {
 		});
 		alert.addInput({
 			type: 'radio',
-			value:'AUD',
-			label:'AUD'
+			value:'VND',
+			label:'VND'
 		});
 		alert.addButton({
       		text: 'OK',
       		handler: data => {
         		//console.log('Selected Data:' + data);
-        
         		this.baseCurrency = (source=="baseCurrency"? data : this.baseCurrency);
         		this.targetCurrency = (source=="targetCurrency"? data : this.targetCurrency);
-        		
         		this.currencySelectAlertOpen = false;
         		//console.log('Updated data:' + this.LCurrency + "-" + this.RCurrency);
         		//console.log('Input Data:' + this.LValue + "-" + this.RValue);
-        		console.log('Result:' + JSON.stringify(this.result));
+        		//console.log('Result:' + JSON.stringify(this.result));
         	}
         });
         alert.present().then(() => {
--- src/providers/currency-exch-provider.ts
+++ src/providers/currency-exch-provider.ts
@@ -13,10 +13,9 @@ export class CurrencyExchProvider {
   data:any;
   baseCur: string;
   targetCur: string;
+  refresh: any;
   constructor(public http: Http) {
-    console.log('Hello CurrencyExchProvider Provider');
-
-    
+    console.log('>>>CurrencyExchProvider executed');  
   }
 
   load() {
@@ -25,14 +24,17 @@ export class CurrencyExchProvider {
     //}
     // Dont have the data yet
     return new Promise(resolve => {
-      console.log('-->' + this.baseCur + this.targetCur);
-      this.http.get('http://api.fixer.io/latest?base='+this.baseCur+'&symbols='+this.targetCur)
+      //console.log('-->' + this.baseCur + this.targetCur);
+      //let queryString = 'http://api.fixer.io/latest?base='+this.baseCur+'&symbols='+this.targetCur
+      let queryString = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22' + this.baseCur + this.targetCur + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+      this.http.get(queryString)
         .map(res => res.json())
         .subscribe(data => {
-          this.data = data
-          //this.data = data.rates.JPY;
+          //this.data = data
+          //this.data = data.rates[this.targetCur];
+          this.data = data.query.results.rate.Rate;
           resolve(this.data);
-          console.log('-->'+JSON.stringify(data));
+          //console.log('-->'+JSON.stringify(data));
         });
     });
 
